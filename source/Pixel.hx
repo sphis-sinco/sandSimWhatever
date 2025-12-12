3b6d81267967a7d05464a9445fa25b057b9810d9
Viwu <sinconsistencia@gmail.com>
2025-12-12 11:21:46 -0600

    pixel

---
+++ source/Pixel.hx
@@ -0,0 +1,26 @@
+import flixel.util.FlxColor;
+import flixel.FlxSprite;
+
+class Pixel extends FlxSprite
+{
+    public var material:Material = STONE;
+
+	override public function new(material:Material = STONE, ?x:Float, ?y:Float)
+	{
+		super(x, y);
+
+        this.material = material;
+		makeGraphic(1, 1, 0xFFFFFF);
+	}
+
+    override function update(elapsed:Float) {
+        super.update(elapsed);
+
+        this.color = cast material;
+    }
+}
+
+enum abstract Material(FlxColor) {
+    var SAND = 0xE6E329;
+    var STONE = 0x535353;
+}
