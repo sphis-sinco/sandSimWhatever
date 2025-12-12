e9c35487267194edc0169b87b61597cbb49ea168
Viwu <sinconsistencia@gmail.com>
2025-12-12 11:31:37 -0600

    pixel shitz

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,5 +1,6 @@
 package;
 
+import flixel.FlxG;
 import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.FlxState;
 
@@ -7,12 +8,19 @@ class PlayState extends FlxState
 {
 	public var pixels:FlxTypedGroup<Pixel>;
 
+	public var curPixel:Pixel;
+
 	override public function create()
 	{
 		super.create();
 
 		pixels = new FlxTypedGroup<Pixel>();
 		add(pixels);
+
+		curPixel = new Pixel(SAND);
+		add(curPixel);
+
+		curPixel.alpha = 0.5;
 	}
 
 	override public function update(elapsed:Float)
@@ -20,8 +28,15 @@ class PlayState extends FlxState
 		super.update(elapsed);
 
 		for (pixel in pixels.members)
-		{
 			performPixelAction(pixel);
+
+		curPixel.setPosition(FlxG.mouse.x, FlxG.mouse.y);
+
+		if (FlxG.keys.justReleased.ENTER)
+		{
+			var newPx:Pixel = new Pixel(curPixel.material, curPixel.x, curPixel.y);
+			if (!getPixelOverlap(newPx))
+				pixels.add(newPx);
 		}
 	}
 
@@ -45,5 +60,11 @@ class PlayState extends FlxState
 			default:
 				// nothin
 		}
+
+		if (pixel.x < 0) pixel.x = 0;
+		if (pixel.x > FlxG.width - pixel.width) pixel.x = FlxG.width - pixel.width;
+
+		if (pixel.y < 0) pixel.y = 0;
+		if (pixel.y > FlxG.width - pixel.width) pixel.y = FlxG.width - pixel.width;
 	}
 }
